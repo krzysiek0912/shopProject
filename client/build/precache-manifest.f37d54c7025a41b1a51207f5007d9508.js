fc42986959ae6ce0b42534eccaff84ae2be82d50
Krzysztof Branecki <krzysiek091290@gmail.com>
2019-12-05 11:15:52 +0100

    Add build folder to heroku

---
+++ client/build/precache-manifest.f37d54c7025a41b1a51207f5007d9508.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5b2edfa32b224a34cea606c2c84ace",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f696138381bb82c04f2",
+    "url": "/static/css/main.00b2128a.chunk.css"
+  },
+  {
+    "revision": "fc35bc529195ec1f42d8",
+    "url": "/static/js/2.491078fa.chunk.js"
+  },
+  {
+    "revision": "3f696138381bb82c04f2",
+    "url": "/static/js/main.7b2ddc64.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
